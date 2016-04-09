c3bf710ed2f9e082737ebbfcc0003dbca84c86b5
Naveed <08es34@gmail.com>
2016-04-09 16:40:41 +0500

    initial setup complete

--- app.js
+++ app.js
@@ -2,17 +2,71 @@
 
 angular.module('myApp', [])
     .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.data = [];
+        $scope.data = [
+            {
+                id: 1,
+                label: "Naveed",
+                children: [
+                    {
+                        id: 1.1,
+                        label: "Angular",
+                        children: [
+                            {
+                                id: 1.1,
+                                label: "Angular"
+                            },
+                            {
+                                id: 1.2,
+                                label: "Laravel"
+                            }
+                        ]
+                    },
+                    {
+                        id: 1.2,
+                        label: "Laravel"
+                    }
+                ]
+            },
+            {
+                id: 2,
+                label: "Ali",
+                children: [
+                    {
+                        id: 2.1,
+                        label: "PHP"
+                    },
+                    {
+                        id: 2.2,
+                        label: "NetSuite"
+                    }
+                ]
+            },
+            {
+                id: 3,
+                label: "Basit",
+                children: [
+                    {
+                        id: 3.1,
+                        label: "Node"
+                    },
+                    {
+                        id: 3.2,
+                        label: "Mongo"
+                    }
+                ]
+            }
+        ];
     }])
 
     .directive('nvdNgTree', function () {
         return {
-            restrict: 'A',
+            restrict: 'E',
+            templateUrl: 'nvd-ng-tree.html',
+            scope: {
+                items: '=items'
+            },
             link: function (scope, elem, attrs) {
-                // watch when the attribute changes
-                scope.$watch(attrs['nvdNgTree'], function (data) {
 
-                });
             }
         };
     });
