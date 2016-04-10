e055c6feb9fa4be4e98fb22b0764f94cf6bfb9e5
Naveed <08es34@gmail.com>
2016-04-10 21:20:40 +0500

    single node functionality complete - collection functionality to go

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ angular.module('myApp', [])
             {
                 id: 1,
                 label: "Naveed",
+                opened: true,
                 children: [
                     {
                         id: 1.1,
                         label: "Angular",
                         children: [
                             {
-                                id: 1.1,
+                                id: 1.11,
                                 label: "Angular"
                             },
                             {
-                                id: 1.2,
-                                label: "Laravel"
+                                id: 1.12,
+                                label: "Laravel",
+                                children: [
+                                    {id: 1.121,label:'elixir'},
+                                    {id: 1.122,label:'policies'}
+                                ]
                             }
                         ]
                     },
@@ -58,7 +63,7 @@ angular.module('myApp', [])
         ];
     }])
 
-    .directive('nvdNgTree',['NvdNgTreeNodeService', function (Node) {
+    .directive('nvdNgTree',['NvdNgTreeService', function (Tree) {
         return {
             restrict: 'E',
             templateUrl: 'nvd-ng-tree.html',
@@ -66,7 +71,7 @@ angular.module('myApp', [])
                 items: '=items'
             },
             link: function (scope, elem, attrs) {
-                scope.nodes = Node.makeNodes(scope.items);
+                scope.tree = new Tree(scope.items);
             }
         };
     }])
@@ -76,15 +81,22 @@ angular.module('myApp', [])
             this.id = null;
             this.label = "";
             this.children = null;
-            this.parent = null;
-            this.selected = false;
+            this.parentId = null;
+            this.checked = false;
             this.opened = false;
+            this.hasCheckedChildren = false;
 
             for ( var prop in data )
                 this[prop] = data[prop];
 
             if(this.children)
-                this.children = Node.makeNodes(this.children);
+            {
+                this.children = Node.makeNodes( this.children );
+                var parentId = this.id;
+                _.map(this.children, function (node) {
+                    node.parentId = parentId;
+                });
+            }
         };
 
         Node.makeNodes = function (items) {
@@ -99,12 +111,75 @@ angular.module('myApp', [])
             this.opened = !this.opened;
         };
 
-        Node.prototype.toggleSelected = function () {
-            this.selected = !this.selected;
+        Node.prototype.toggleChecked = function (collection) {
+            this.setChecked(!this.checked);
+            // update parent status
+            this.updateParentCheckedStatus(collection);
+        };
+
+        Node.prototype.setChecked = function (value) {
+            this.checked = value;
+            if(!value) this.hasCheckedChildren = false;
+            // toggle-check for all children
+            var thisNode = this;
+            if (this.children) {
+                _.map(thisNode.children, function (childNode) {
+                    childNode.setChecked(value);
+                });
+            }
+        };
+
+        Node.prototype.updateParentCheckedStatus = function (collection) {
+            var thisNode = this;
+            if (thisNode.parentId) {
+                console.log("the node has parent");
+                var parentNode = thisNode.getParent(collection);
+                console.log(parentNode);
+                var allChecked = true;
+                var someChecked = false;
+                _.map(parentNode.children, function (childNode) {
+                    childNode.checked ? someChecked = true : allChecked = false;
+                    if( childNode.hasCheckedChildren )
+                        someChecked = true;
+                });
+                parentNode.checked = allChecked;
+                parentNode.hasCheckedChildren = someChecked;
+
+                if(parentNode.parentId)
+                    parentNode.updateParentCheckedStatus(collection);
+            }
+        };
+
+        Node.prototype.getParent = function (collection) {
+            var thisNode = this;
+            var secondaryCollection = [];
+            var result = _.find(collection, function (node) {
+                if(node.children)
+                    secondaryCollection = _.union(secondaryCollection,node.children);
+                return node.id == thisNode.parentId;
+            });
+
+            if( !result && secondaryCollection )
+                return thisNode.getParent(secondaryCollection);
+
+            return result;
         };
 
         // build the api and return it
         return Node;
-
     })
+
+    .factory('NvdNgTreeService',['NvdNgTreeNodeService', function (Node) {
+        var Tree = function (items) {
+            var thisTree;
+            this.nodes = Node.makeNodes(items);
+        };
+
+        Tree.prototype.getChecked = function () {
+
+        };
+
+        // build the api and return it
+        return Tree;
+    }])
 ;
